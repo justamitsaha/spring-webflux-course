6371ab14f3a0e31e8aeed218ee057f1ba4ab6b64
justamitsaha <justamitsaha@gmail.com>
2024-04-18 06:37:36 +0530

    Reactive router

--- webflux-demo/src/main/java/com/vinsguru/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/vinsguru/webfluxdemo/config/RouterConfig.java
@@ -17,26 +17,48 @@ public class RouterConfig {
     private RequestHandler requestHandler;
 
     @Bean
-    public RouterFunction<ServerResponse> highLevelRouter(){
+    public RouterFunction<ServerResponse> highLevelRouter() {
         return RouterFunctions.route()
-                              .path("square", this::serverResponseRouterFunction)
+                .path("route", this::checkOperation)
                 .build();
     }
 
-   // @Bean
-    private RouterFunction<ServerResponse> serverResponseRouterFunction(){
+    private RouterFunction<ServerResponse> checkOperation() {
         return RouterFunctions.route()
-                              .GET("/{input}", RequestPredicates.path("/1?"), requestHandler::squareHandler)
+                .path("square", this::serverResponseSquareFunction)
+                .path("table", this::serverResponseTableFunction)
+                .path("multiply", this::serverResponseMultiplyFunction)
+                .build();
+    }
+
+    // @Bean
+    private RouterFunction<ServerResponse> serverResponseSquareFunction() {
+        return RouterFunctions.route()
+                .GET("/{input}", RequestPredicates.path("/1?"), requestHandler::squareHandler)
                 .GET("{input}", req -> ServerResponse.badRequest().bodyValue("only 10-19 allowed"))
-                .GET("table/{input}", requestHandler::tableHandler)
-                .GET("table/{input}/stream", requestHandler::tableStreamHandler)
+                .onError(InputValidationException.class, exceptionHandler())
+                .build();
+    }
+
+    private RouterFunction<ServerResponse> serverResponseTableFunction() {
+        return RouterFunctions.route()
+                .GET("{input}", RequestPredicates.path(".*[^0]$"), requestHandler::tableHandler)
+                .GET("{input}", request -> ServerResponse.badRequest().bodyValue("For Number ending with Zero don't use API"))
+                .GET("{input}/stream", RequestPredicates.path(".*[^0]$"), requestHandler::tableStreamHandler)
+                .GET("{input}/stream", request -> ServerResponse.badRequest().bodyValue("For Number ending with Zero don't use API"))
+                .onError(InputValidationException.class, exceptionHandler())
+                .build();
+    }
+
+    private RouterFunction<ServerResponse> serverResponseMultiplyFunction() {
+        return RouterFunctions.route()
                 .POST("multiply", requestHandler::multiplyHandler)
                 .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
                 .onError(InputValidationException.class, exceptionHandler())
                 .build();
     }
 
-    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler(){
+    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler() {
         return (err, req) -> {
             InputValidationException ex = (InputValidationException) err;
             InputFailedValidationResponse response = new InputFailedValidationResponse();
--- webflux-demo/src/main/java/com/vinsguru/webfluxdemo/controller/ReactiveMathController.java
+++ webflux-demo/src/main/java/com/vinsguru/webfluxdemo/controller/ReactiveMathController.java
@@ -5,6 +5,7 @@ import com.vinsguru.webfluxdemo.dto.Response;
 import com.vinsguru.webfluxdemo.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.AbstractJackson2Encoder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,27 +20,30 @@ public class ReactiveMathController {
     private ReactiveMathService mathService;
 
     @GetMapping("square/{input}")
-    public Mono<Response> findSquare(@PathVariable int input){
+    public Mono<Response> findSquare(@PathVariable int input) {
         return this.mathService.findSquare(input)
-                        .defaultIfEmpty(new Response(-1));
+                .defaultIfEmpty(new Response(-1));
     }
 
     /*
-    Altough this endpoint is returning Flux, but to UI it will be returned as JSON, 
-    So unless the user recieves entire response it can't do anything.
-    If we stop the request in browser we will see no logs in Serevr, that is the process will stop
-    So the browser is subscribing to it, once it subscribtions ends the process on backedn ends
-    In some browsers it will continoiusly give data, but its still part of a JSON can't do anything 
-    untill entire JSOM is recieved
+    Although this endpoint is returning Flux, but to UI it will be returned as JSON,
+    So unless the user receives entire response it can't do anything.
+    In some browsers it will continuously give data, but it is still part of a JSON and UI can't do anything
+    until entire JSON is received
+    Since we are not defining that it produces stream Spring with help of AbstractJackson2Encoder is converting
+    it to a list and retuning
     */
     @GetMapping("table/{input}")
-    public Flux<Response> multiplicationTable(@PathVariable int input){
+    public Flux<Response> multiplicationTable(@PathVariable int input) {
+        //AbstractJackson2Encoder is converting to Mono of List 
         return this.mathService.multiplicationTable(input);
     }
 
     /*
-
-    */
+    This API returns a stream
+     If we stop the request in browser we will see no logs in Server, that is the process will stop
+    So the browser is subscribing to it, once the subscriptions ends the process on backend ends
+     */
     @GetMapping(value = "table/{input}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Response> multiplicationTableStream(@PathVariable int input) {
         return this.mathService.multiplicationTable(input);
@@ -47,7 +51,7 @@ public class ReactiveMathController {
 
     @PostMapping("multiply")
     public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDto> requestDtoMono,
-                                   @RequestHeader Map<String, String> headers){
+                                   @RequestHeader Map<String, String> headers) {
         System.out.println(headers);
         return this.mathService.multiply(requestDtoMono);
     }
