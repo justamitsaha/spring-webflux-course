ebc1230b502d840fbefa40ee0c810bb6ee449d7f
= <=>
2025-05-09 17:37:46 +0530

    doc update

--- 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec02/AbstractTest.java
+++ 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec02/AbstractTest.java
@@ -3,7 +3,8 @@ package com.vinsguru.playground.tests.sec02;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-        "sec=sec02"
+        "sec=sec02",
+        "logging.level.org.springframework.r2dbc=DEBUG"
 })
 public abstract class AbstractTest {
 }
